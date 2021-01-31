28a9479416f534cf52cd743f0d6934eb2e541ecf
Dang Nguyen <dangns229@gmail.com>
2021-01-31 00:09:34 -0800

    Hookup to CharacterIndex toolbar

--- Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
@@ -60,9 +60,9 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public async void AddItem_Clicked(object sender, EventArgs e)
+        public async void AddCharacter_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new NavigationPage(new ItemCreatePage()));
+            await Navigation.PushModalAsync(new NavigationPage(new CharacterCreatePage()));
         }
 
         /// <summary>
