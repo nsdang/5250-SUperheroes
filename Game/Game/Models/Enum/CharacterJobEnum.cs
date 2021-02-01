0290665a48116f4f429164163897882846809c0f
Dang Nguyen <dangns229@gmail.com>
2021-01-31 17:20:20 -0800

    Added new Character and Monster Jobs in CharacterJobEnum.cs

--- Game/Game/Models/Enum/CharacterJobEnum.cs
+++ Game/Game/Models/Enum/CharacterJobEnum.cs
@@ -7,7 +7,7 @@
     public enum CharacterJobEnum
     {
         // Not specified
-        Unknown = 0,    
+        Unknown = -1,
 
         // Fighters hit hard and have fight abilities
         Fighter = 10,
@@ -15,6 +15,27 @@
         // Clerics defend well and have buff abilities
         Cleric = 12,
 
+        // New Heroes' Jobs
+        Flying = 0,
+
+        Psychic = 1,
+
+        Physical = 2,
+
+        Light = 3,
+
+        Dark = 4,
+
+        // New Monsters' Jobs
+        Carpenter = 5,
+
+        Accountant = 6,
+
+        McDonaldsEmployee = 7,
+
+        Vet = 8,
+
+        Boss = 9,
     }
 
     /// <summary>
@@ -34,12 +55,50 @@
 
             switch (value)
             {
-                case CharacterJobEnum.Fighter:
-                    Message = "Fighter";
+                //case CharacterJobEnum.Fighter:
+                //    Message = "Fighter";
+                //    break;
+
+                //case CharacterJobEnum.Cleric:
+                //    Message = "Cleric";
+                //    break;
+
+                case CharacterJobEnum.Flying:
+                    Message = "Flying";
                     break;
 
-                case CharacterJobEnum.Cleric:
-                    Message = "Cleric";
+                case CharacterJobEnum.Psychic:
+                    Message = "Psychic";
+                    break;
+                case CharacterJobEnum.Physical:
+                    Message = "Physical";
+                    break;
+
+                case CharacterJobEnum.Light:
+                    Message = "Light";
+                    break;
+                case CharacterJobEnum.Dark:
+                    Message = "Dark";
+                    break;
+
+                case CharacterJobEnum.Carpenter:
+                    Message = "Carpenter";
+                    break;
+
+                case CharacterJobEnum.Accountant:
+                    Message = "Accountant";
+                    break;
+
+                case CharacterJobEnum.Vet:
+                    Message = "Vet";
+                    break;
+
+                case CharacterJobEnum.McDonaldsEmployee:
+                    Message = "McDonaldsEmployee";
+                    break;
+
+                case CharacterJobEnum.Boss:
+                    Message = "Boss";
                     break;
 
                 case CharacterJobEnum.Unknown:
