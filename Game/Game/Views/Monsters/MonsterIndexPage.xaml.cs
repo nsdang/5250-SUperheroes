db54131de60d6c067951319d450bba60a32a1f4b
cangelica <cangelica1@seattleu.edu>
2021-02-04 12:05:36 -0800

    Added MonsterIndexPage to Project.

---
+++ Game/Game/Views/Monsters/MonsterIndexPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.Models;
+using Game.ViewModels;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Index Page
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+    [DesignTimeVisible(false)] 
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonsterIndexPage : ContentPage
+    {
+        // The view model, used for data binding
+        readonly MonsterIndexViewModel ViewModel = MonsterIndexViewModel.Instance;
+
+        // Empty Constructor for UTs
+        public MonsterIndexPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the ItemIndexView Model
+        /// </summary>
+        public MonsterIndexPage()
+        {
+            InitializeComponent();
+
+            BindingContext = ViewModel;
+        }
+
+        /// <summary>
+        /// The image clicked by should show the corresponding character read page. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void OnImageClicked(object sender, EventArgs e)
+        {
+            ImageButton img = sender as ImageButton;
+            CharacterModel selected = img.BindingContext as CharacterModel;
+            if (selected == null)
+            {
+                return;
+            }
+
+            // Open the Read Page
+            await Navigation.PushAsync(new CharacterReadPage(new GenericViewModel<CharacterModel>(selected)));
+
+        }
+
+        /// <summary>
+        /// Call to Add a new record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void AddCharacter_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new CharacterCreatePage()));
+        }
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
+        }
+    }
+}
