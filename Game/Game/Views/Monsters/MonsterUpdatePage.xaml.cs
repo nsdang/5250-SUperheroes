3f54ff39f1133e5f6e67479146dd034115c28bf5
Dang Nguyen <dangns229@gmail.com>
2021-02-07 16:53:28 -0800

    Created MonsterUpdatePage under Monster folder. Updated Model and names

---
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.ViewModels;
+using Game.Models;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Item Update Page
+    /// </summary>
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonsterUpdatePage : ContentPage
+    {
+        // View Model for Item
+        public readonly GenericViewModel<MonsterModel> ViewModel;
+
+        // Empty Constructor for Tests
+        public MonsterUpdatePage(bool UnitTest){ }
+
+        /// <summary>
+        /// Constructor that takes and existing data item
+        /// </summary>
+        public MonsterUpdatePage(GenericViewModel<MonsterModel> data)
+        {
+            InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+
+            this.ViewModel.Title = "Update " + data.Title;
+
+        }
+
+        /// <summary>
+        /// Save calls to Update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Save_Clicked(object sender, EventArgs e)
+        {
+            // If the image in the data box is empty, use the default one..
+            if (string.IsNullOrEmpty(ViewModel.Data.ImageURI))
+            {
+                ViewModel.Data.ImageURI = Services.ItemService.DefaultImageURI;
+            }
+
+            MessagingCenter.Send(this, "Update", ViewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Cancel and close this page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
