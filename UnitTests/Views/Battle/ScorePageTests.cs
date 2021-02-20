187c24f0a0136d51e08fc6bf6a177d6419182cc5
AjerLodhi <ajer2325172@gmail.com>
2021-02-19 21:35:18 -0800

    Partial change, Uncommenting previously commented not running tests in file ScorePageTests.cs

--- UnitTests/Views/Battle/ScorePageTests.cs
+++ UnitTests/Views/Battle/ScorePageTests.cs
@@ -40,7 +40,6 @@ namespace UnitTests.Views
             Application.Current = null;
         }
 
-        /*
         [Test]
         public void ScorePage_Constructor_Default_Should_Pass()
         {
@@ -177,6 +176,5 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
-        */
     }
 }
