245fb331524d5363f716a97c61c90b42732a1790
Dang Nguyen <dnguyen1@seattleu.edu>
2021-02-22 00:18:37 -0800

    Added WebGlobalsModelTests.cs under folder Models to test WebGlobalsModel.cs

---
+++ UnitTests/Models/WebGlobalsModelTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class WebGlobalsModelTests
+    {
+        [Test]
+        public void WebGlobalsModel_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("https://itemgetpost.azurewebsites.net/API/", WebGlobalsModel.WebSiteAPIURL);
+        }
+    }
+}
