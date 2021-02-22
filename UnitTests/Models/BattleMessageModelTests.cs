dc60a938dc1a8bd753c83b8df224dcc7d59eefbd
Dang Nguyen <dnguyen1@seattleu.edu>
2021-02-22 13:50:58 -0800

    Added BattleMessageModel_ClearMessage_Should_Pass in BattleMessageModelTests.cs

--- UnitTests/Models/BattleMessageModelTests.cs
+++ UnitTests/Models/BattleMessageModelTests.cs
@@ -240,5 +240,40 @@ namespace UnitTests.Models
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [Test]
+        public void BattleMessageModel_ClearMessage_Should_Pass()
+        {
+            // Arrange
+            var result = new BattleMessagesModel
+            {
+                PlayerType = PlayerTypeEnum.Character,
+                HitStatus = HitStatusEnum.Hit,
+                AttackerName = "CaptainSU"
+            };
+
+            // Act
+            result.ClearMessages();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(PlayerTypeEnum.Unknown, result.PlayerType);
+
+            Assert.AreEqual(HitStatusEnum.Unknown, result.HitStatus);
+
+            Assert.AreEqual(string.Empty, result.AttackerName);
+            Assert.AreEqual(string.Empty, result.TargetName);
+            Assert.AreEqual(string.Empty, result.AttackStatus);
+            Assert.AreEqual(string.Empty, result.TurnMessage);
+            Assert.AreEqual(string.Empty, result.TurnMessageSpecial);
+            Assert.AreEqual(string.Empty, result.LevelUpMessage);
+
+            Assert.AreEqual(0, result.DamageAmount);
+            Assert.AreEqual(0, result.CurrentHealth);
+
+            Assert.AreEqual(@"<html><body bgcolor=""#E8D0B6""><p>", result.htmlHead);
+            Assert.AreEqual(@"</p></body></html>", result.htmlTail);
+        }
     }
 }
