bab528dd390fecf0eda04e4c4565e594b305ed39
Dang Nguyen <dnguyen1@seattleu.edu>
2021-02-22 00:09:54 -0800

    Added BattleSettingsModelTest.cs file to test BattleSettingsModel.cs under folder Model.

---
+++ UnitTests/Models/BattleSettingsModelTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class BattleSettingsModelTests
+    {
+        [Test]
+        public void BattleSettingsModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new BattleSettingsModel();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Simple Next", result.BattleModeEnum.ToMessage());
+        }
+    }
+}
