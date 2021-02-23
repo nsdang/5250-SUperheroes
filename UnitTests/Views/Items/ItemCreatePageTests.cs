71fe70758702d5ad13b688c48533de3a1c784e54
cangelica <cangelica1@seattleu.edu>
2021-02-22 18:18:11 -0800

    Added ItemCreatePage_Save_Clicked_Null_Should_Pass() to ItemCreatePageTests.cs

--- UnitTests/Views/Items/ItemCreatePageTests.cs
+++ UnitTests/Views/Items/ItemCreatePageTests.cs
@@ -101,6 +101,20 @@ namespace UnitTests.Views
         }
 
         [Test]
+        public void ItemCreatePage_Save_Clicked_Null_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
         public void ItemCreatePage_OnBackButtonPressed_Valid_Should_Pass()
         {
             // Arrange
