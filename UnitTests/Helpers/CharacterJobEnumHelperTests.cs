25fdcacb5fc7652d2973a4d299b6c0da579c316f
Dang Nguyen <dnguyen1@seattleu.edu>
2021-02-22 17:27:55 -0800

    Added CharacterJobEnumHelper_GetListHeroJob_Should_Pass() in CharacterJobEnumHelperTests.cs

--- UnitTests/Helpers/CharacterJobEnumHelperTests.cs
+++ UnitTests/Helpers/CharacterJobEnumHelperTests.cs
@@ -9,19 +9,19 @@ namespace UnitTests.Helpers
     [TestFixture]
     class CharacterJobEnumHelperTests
     {
-        /*
+        
         [Test]
-        public void CharacterJobEnumHelper_GetJobList_Should_Pass()
+        public void CharacterJobEnumHelper_GetListHeroJob_Should_Pass()
         {
             // Arrange
 
             // Act
-            var result = CharacterJobEnumHelper.GetJobList;
+            var result = CharacterJobEnumHelper.GetListHeroJob;
 
             // Assert
-            Assert.AreEqual(2,result.Count());
+            Assert.AreEqual(5,result.Count());
 
             // Assert
-        }*/
+        }
     }
 }
